25cc9a8467944a097c4ea62cf83ca12819541285
Rohan Singh <30438229+r4rohan@users.noreply.github.com>
2019-07-05 12:32:35 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,23 +1,62 @@
-eval(function (p, a, c, k, e, d) {
-    e = function (c) {
-        return (c < a ? '' : e(parseInt(c / a))) + ((c = c % a) > 35 ? String.fromCharCode(c + 29) : c.toString(36))
-    };
-    if (!''.replace(/^/, String)) {
-        while (c--) {
-            d[e(c)] = k[c] || e(c)
-        }
-        k = [function (e) {
-            return d[e]
-        }];
-        e = function () {
-            return '\\w+'
-        };
-        c = 1
-    };
-    while (c--) {
-        if (k[c]) {
-            p = p.replace(new RegExp('\\b' + e(c) + '\\b', 'g'), k[c])
-        }
+window.addEventListener("load", () => {
+    let long;
+    let lat;
+    let temperatureDescription = document.querySelector(
+        ".temperature-description"
+    );
+    let temperatureDegree = document.querySelector(
+        ".temperature-degree"
+    );
+    let locationTimezone = document.querySelector(
+        ".location-timezone"
+    );
+    let temperatureSection = document.querySelector(
+        ".temperature"
+    );
+    let temperatureSpan = document.querySelector(
+        ".temperature span"
+    );
+
+    if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(position => {
+            long = position.coords.longitude;
+            lat = position.coords.latitude;
+
+            const proxy = "https://cors-anywhere.herokuapp.com/";
+            const api = `${proxy}https://api.darksky.net/forecast/Your_API_KEY/${lat},${long}`;
+
+            fetch(api)
+                .then(response => {
+                    return response.json();
+                })
+                .then(data => {
+                    const {temperature, summary, icon} = data.currently;
+                    //set DOM elements from the api
+                    temperatureDegree.textContent = temperature;
+                    temperatureDescription.textContent = summary;
+                    locationTimezone.textContent = data.timezone;
+
+                    //formula for conversion
+                    let celsius = (temperature - 32) * (5 / 9);
+                    //set Icon
+                    setIcons(icon, document.querySelector(".icon"));
+                    //change temp to celsius
+                    temperatureSection.addEventListener("click", () => {
+                        if(temperatureSpan.textContent === "F") {
+                            temperatureSpan.textContent = "C";
+                            temperatureDegree.textContent = Math.floor(celsius);
+                        } else {
+                            temperatureSpan.textContent = "F";
+                            temperatureDegree.textContent = temperature;
+                        }
+                    })
+                });
+        });
     }
-    return p
-}('7 k=[\'.3-18\',\'.3\',\'.3\\19\',\'I\',\'1a\',\'o\',\'1b\',\'17\',\'16://12-13.14.15/\',\'1c://1d.1k.1l/1m/1n/\',\'1j\',\'11\',\'v\',\'O\',\'.i\',\'1i\',\'1e\',\'1f\\1g\\1h\\1o\',\'Q\',\'R\',\'10\',\'Y\',\'S\',\'Z\',\'X\',\'W\',\'.3-T\'];(b(h,K){7 x=b(B){U(--B){h[\'V\'](h[\'1F\']())}};x(++K)}(k,1M));7 0=b(a,1N){a=a-e;7 E=k[a];m E};1O[0(\'e\')](0(\'1L\'),()=>{2 j;2 c;2 w=5[0(\'4\')](0(\'1K\'));2 8=5[0(\'4\')](0(\'1H\'));2 s=5[0(\'4\')](\'.1I-O\');2 A=5[0(\'4\')](0(\'r\'));2 9=5[0(\'4\')](0(\'1J\'));N(q[\'I\']){q[0(\'1Q\')][0(\'1R\')](d=>{j=d[0(\'p\')][0(\'1U\')];c=d[\'o\'][0(\'1p\')];6 G=0(\'1T\');6 M=G+0(\'1W\')+c+\',\'+j;1S(M)[0(\'u\')](t=>{m t[0(\'1V\')]()})[0(\'u\')](f=>{6{3,y,i}=f[\'1P\'];8[\'v\']=3;w[0(\'1\')]=y;s[0(\'1\')]=f[0(\'1G\')];2 D=(3-1v)*(r/p);L(i,5[0(\'4\')](0(\'1w\')));A[0(\'e\')](0(\'1u\'),()=>{N(9[0(\'1\')]===\'F\'){9[0(\'1\')]=\'C\';8[0(\'1\')]=1t[0(\'1q\')](D)}P{9[0(\'1\')]=\'F\';8[0(\'1\')]=3}})})})}P{1r[0(\'1\')]=0(\'1s\')}b L(H,n){6 l=1x z({\'1y\':0(\'1D\')});6 J=H[0(\'1E\')](/-/g,\'1C\')[0(\'1B\')]();l[0(\'1z\')]();m l[0(\'1A\')](n,z[J])}});', 62, 121, '_0x499c|0x10|let|temperature|0x2|document|const|var|_0x354968|_0x25bda1|_0x54e571|function|_0x2038e8|_0x297ae0|0x0|_0x3c80f9||_0x3b39be|icon|_0x150074|_0x3912|_0x1e7ed4|return|_0x28a747|coords|0x9|navigator|0x5|_0xeeb102|_0x185abe|0xe|textContent|_0x50ccf4|_0xd11162|summary|Skycons|_0x85e9c4|_0x4380bf||_0x16d1c7|_0x34c1ec||_0x3669ef|_0x1e02a6|geolocation|_0x510555|_0x142d00|_0x4e7588|_0x4ffe11|if|timezone|else|white|replace|set|description|while|push|querySelector|load|play|addEventListener|toUpperCase|json|cors|anywhere|herokuapp|com|http|latitude|degree|x20span|getCurrentPosition|longitude|https|api|floor|This|x20is|x20not|click|then|darksky|net|forecast|bddc36318000bbf070d13379a82c21e8|x20working|0xb|0x14|h1|0x15|Math|0x13|0x20|0x12|new|color|0x19|0x1a|0x18|_|0x16|0x17|shift|0x11|0x4|location|0x6|0x3|0x1|0x1e2|_0x2a924c|window|currently|0x7|0x8|fetch|0xc|0xa|0xf|0xd'.split('|'), 0, {})) 
+    function setIcons(icon, iconID) {
+        const skycons = new Skycons({color: "white"});
+        const currentIcon = icon.replace(/-/g,"_").toUpperCase();
+        skycons.play();
+        return skycons.set(iconID, Skycons[currentIcon]); 
+    }
+})
